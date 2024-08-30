0e8cc6d873428b7077a13591a0268f4448607f0a
Yelyzaveta <yelyzavetasokolova71@gmail.com>
2024-08-31 00:16:06 +0200

    11/0

---
+++ src/main.js
@@ -0,0 +1,105 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+function createMarkup(arr) {
+  return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+    `<li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+              width="360"
+            />
+          </a>
+          <div class="thumb-block">
+            <div class="block">
+              <h2 class="tittle">Likes</h2>
+              <p class="amount">${likes}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Views</h2>
+              <p class="amount">${views}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Comments</h2>
+              <p class="amount">${comments}</p>
+            </div>
+            <div class="block">
+              <h2 class="tittle">Downloads</h2>
+              <p class="amount">${downloads}</p>
+            </div>
+          </div>
+        </li>`)
+    .join('');
+}
+
+const formSearch = document.querySelector('.js-search');
+const listImages = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+loader.style.display = 'none';
+formSearch.addEventListener('submit', onSearch);
+
+function onSearch(event) {
+  event.preventDefault();
+  listImages.innerHTML = '';
+  loader.style.display = 'block';
+
+  const inputValue = event.target.elements.search.value;
+
+  getPictures(inputValue)
+    .then(data => {
+      loader.style.display = 'none';
+
+      if (!data.hits.length) {
+        iziToast.error({
+          title: '❌',
+          message: 'Sorry, there are no images matching your search query. Please, try again!',
+        });
+      }
+
+      listImages.innerHTML = ("beforeend", createMarkup(data.hits));
+
+      const refreshPage = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      refreshPage.refresh();
+
+      formSearch.reset();
+    })
+    .catch((err) => {
+      loader.style.display = 'none';
+      console.log(err);
+    });
+}
+
+function getPictures(name) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const KEY = '32552782-0d4c86680018457e820f20492';
+
+  if (name.includes(' ')) {
+    name.replace(/\s+/g, '+');
+  }
+
+  const searchParams = new URLSearchParams({
+    key: KEY,
+    q: name,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  })
+
+  return fetch(`${BASE_URL}?${searchParams}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    })
+}
+
