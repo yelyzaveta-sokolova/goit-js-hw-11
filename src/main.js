0422a4ed060b239cc45fec46f3d812b780e26f34
Yelyzaveta <yelyzavetasokolova71@gmail.com>
2024-09-01 17:15:36 +0200

    11.3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,93 @@
+// У файлі main.js напиши всю логіку роботи додатка.
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const formSearch = document.querySelector('.js-search');
+const listImages = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+
+loader.style.display = 'none';
+formSearch.addEventListener('submit', onSearch);
+
+function onSearch(event) {
+  event.preventDefault();
+  listImages.innerHTML = '';
+  loader.style.display = 'block';
+
+  const inputValue = event.target.elements.search.value;
+
+  getPictures(inputValue)
+    .then(data => {
+      loader.style.display = 'none';
+
+      if (!data.hits.length) {
+        iziToast.error({
+          title: 'Error',
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+
+listImages.innerHTML = ("beforeend", createMarkup(data.hits));
+
+      const refreshPage = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      refreshPage.refresh();
+
+      formSearch.reset();
+    })
+    .catch((err) => {
+      loader.style.display = 'none';
+      console.log(err);
+    });
+}
+
+
+
+const valueInput = document.querySelector('.input-search');
+function validateInput(valueInput) {
+   
+    const trimmedValue = valueInput.trim();
+
+    if (trimmedValue === '') {
+        alert("Input пустой или содержит только пробелы");
+        return false;
+    } else {
+        console.log("Input корректный");
+        return true;
+    }
+}
+
+function getPictures(name) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const KEY = '32552782-0d4c86680018457e820f20492';
+
+  if (name.includes(' ')) {
+    name.replace(/\s+/g, '+');
+  }
+
+  const searchParams = new URLSearchParams({
+    key: KEY,
+    q: name,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  })
+
+  return fetch(`${BASE_URL}?${searchParams}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    })
+}
 
 function createMarkup(arr) {
   return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
@@ -36,70 +122,7 @@ function createMarkup(arr) {
     .join('');
 }
 
-const formSearch = document.querySelector('.js-search');
-const listImages = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
 
-loader.style.display = 'none';
-formSearch.addEventListener('submit', onSearch);
 
-function onSearch(event) {
-  event.preventDefault();
-  listImages.innerHTML = '';
-  loader.style.display = 'block';
 
-  const inputValue = event.target.elements.search.value;
-
-  getPictures(inputValue)
-    .then(data => {
-      loader.style.display = 'none';
-
-      if (!data.hits.length) {
-        iziToast.error({
-          title: '❌',
-          message: 'Sorry, there are no images matching your search query. Please, try again!',
-        });
-      }
-
-      listImages.innerHTML = ("beforeend", createMarkup(data.hits));
-
-      const refreshPage = new SimpleLightbox('.gallery a', {
-        captions: true,
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      refreshPage.refresh();
-
-      formSearch.reset();
-    })
-    .catch((err) => {
-      loader.style.display = 'none';
-      console.log(err);
-    });
-}
-
-function getPictures(name) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const KEY = '32552782-0d4c86680018457e820f20492';
-
-  if (name.includes(' ')) {
-    name.replace(/\s+/g, '+');
-  }
-
-  const searchParams = new URLSearchParams({
-    key: KEY,
-    q: name,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  })
-
-  return fetch(`${BASE_URL}?${searchParams}`)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-      return res.json();
-    })
-}
 
