25164601b8dee5f763f32ac4e24d13001736af41
Yelyzaveta <yelyzavetasokolova71@gmail.com>
2024-09-01 19:01:15 +0200

    11.4

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,27 +1,28 @@
 // У файлі pixabay-api.js зберігай функції для HTTP-запитів.
 
 
-// function getPictures(name) {
-//   const BASE_URL = 'https://pixabay.com/api/';
-//   const KEY = '32552782-0d4c86680018457e820f20492';
 
-//   if (name.includes(' ')) {
-//     name.replace(/\s+/g, '+');
-//   }
+export function getPictures(name) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const KEY = '32552782-0d4c86680018457e820f20492';
 
-//   const searchParams = new URLSearchParams({
-//     key: KEY,
-//     q: name,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//   })
+  if (name.includes(' ')) {
+    name = name.replace(/\s+/g, '+');
+  }
 
-//   return fetch(`${BASE_URL}?${searchParams}`)
-//     .then(res => {
-//       if (!res.ok) {
-//         throw new Error(res.statusText);
-//       }
-//       return res.json();
-//     })
-// }
+  const searchParams = new URLSearchParams({
+    key: KEY,
+    q: name,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  return fetch(`${BASE_URL}?${searchParams}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    });
+}
--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { getPictures } from './js/pixabay-api.js';
+import { createMarkup } from './js/render-functions.js';
+
 const formSearch = document.querySelector('.js-search');
 const listImages = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
-
 loader.style.display = 'none';
+
 formSearch.addEventListener('submit', onSearch);
 
 function onSearch(event) {
@@ -19,6 +22,11 @@ function onSearch(event) {
   loader.style.display = 'block';
 
   const inputValue = event.target.elements.search.value;
+
+  if (!validateInput(inputValue)) {
+    loader.style.display = 'none';
+    return;
+  }
 
   getPictures(inputValue)
     .then(data => {
@@ -29,100 +37,33 @@ function onSearch(event) {
           title: 'Error',
           message: 'Sorry, there are no images matching your search query. Please try again!',
         });
+      } else {
+        listImages.innerHTML = createMarkup(data.hits);
+
+        const refreshPage = new SimpleLightbox('.gallery a', {
+          captions: true,
+          captionsData: 'alt',
+          captionDelay: 250,
+        });
+        refreshPage.refresh();
       }
-
-listImages.innerHTML = ("beforeend", createMarkup(data.hits));
-
-      const refreshPage = new SimpleLightbox('.gallery a', {
-        captions: true,
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      refreshPage.refresh();
 
       formSearch.reset();
     })
-    .catch((err) => {
+    .catch(err => {
       loader.style.display = 'none';
       console.log(err);
     });
 }
 
-
-
-const valueInput = document.querySelector('.input-search');
 function validateInput(valueInput) {
-   
-    const trimmedValue = valueInput.trim();
+  const trimmedValue = valueInput.trim();
 
-    if (trimmedValue === '') {
-        alert("Input пустой или содержит только пробелы");
-        return false;
-    } else {
-        console.log("Input корректный");
-        return true;
-    }
-}
-
-function getPictures(name) {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const KEY = '32552782-0d4c86680018457e820f20492';
-
-  if (name.includes(' ')) {
-    name.replace(/\s+/g, '+');
+  if (trimmedValue === '') {
+    alert("Кажется, вы забыли указать, какое фото вы хотите найти :)");
+    return false;
+  } else {
+    console.log("Input корректный");
+    return true;
   }
-
-  const searchParams = new URLSearchParams({
-    key: KEY,
-    q: name,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  })
-
-  return fetch(`${BASE_URL}?${searchParams}`)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-      return res.json();
-    })
 }
-
-function createMarkup(arr) {
-  return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-    `<li class="gallery-item">
-          <a class="gallery-link" href="${largeImageURL}">
-            <img
-              class="gallery-image"
-              src="${webformatURL}"
-              alt="${tags}"
-              width="360"
-            />
-          </a>
-          <div class="thumb-block">
-            <div class="block">
-              <h2 class="tittle">Likes</h2>
-              <p class="amount">${likes}</p>
-            </div>
-            <div class="block">
-              <h2 class="tittle">Views</h2>
-              <p class="amount">${views}</p>
-            </div>
-            <div class="block">
-              <h2 class="tittle">Comments</h2>
-              <p class="amount">${comments}</p>
-            </div>
-            <div class="block">
-              <h2 class="tittle">Downloads</h2>
-              <p class="amount">${downloads}</p>
-            </div>
-          </div>
-        </li>`)
-    .join('');
-}
-
-
-
-
-
